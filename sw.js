cce9cf32bdb6d8b2cb6a8f010542e48f1bfed585
javss <javier.ramirez92@yahoo.com>
2026-04-07 18:40:36 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE='habitos-rpg-v3';
-const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>caches.match('./index.html'))));});
-self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.matchAll({type:'window'}).then(cls=>{const c=cls.find(x=>x.url.includes('index.html'));if(c)return c.focus();return clients.openWindow('./index.html');}));});
